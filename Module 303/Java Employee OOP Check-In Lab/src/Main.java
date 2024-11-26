3b68b5a674657fea715cf4012f5dc73e561ed490
mjbae11 <mikejbae@gmail.com>
2024-11-26 12:24:29 -0800

    finished check-in lab

---
+++ Module 303/Java Employee OOP Check-In Lab/src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        Employee dude1 = new Employee("Mike", "Bae", "something", 0.50);
+        Employee dude2 = new Employee("Jorge", "Alvarez", "something", 65.99);
+        Employee dude3 = new Employee("DR", "Martin", "the second", 5.00);
+        System.out.println(dude1.calculateWeeklySalary());
+        System.out.println(dude2.calculateWeeklySalary());
+        System.out.println(dude3.calculateWeeklySalary());
+        System.out.println(dude1);
+        System.out.println(dude2);
+        System.out.println(dude3);
+        System.out.println(dude1.equals(dude2));
+    }
+
+}
