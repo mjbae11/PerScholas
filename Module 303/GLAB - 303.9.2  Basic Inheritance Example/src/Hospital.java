d502c8e85f8aff2b69f35c19cae7af9a7e153b47
mjbae11 <mikejbae@gmail.com>
2024-11-27 16:30:22 -0800

    finished glab basic inheritance

---
+++ Module 303/GLAB - 303.9.2  Basic Inheritance Example/src/Hospital.java
@@ -0,0 +1,9 @@
+public class Hospital
+{
+    public static void main(String[] args)
+    {
+        Surgeon surgeon = new Surgeon();
+        surgeon.Doctor_Details();
+        surgeon.Surgeon_Details();
+    }
+}
