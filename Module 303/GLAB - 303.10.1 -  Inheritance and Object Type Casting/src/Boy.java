11ef2b2f13cee654833e02162112b938008bddb9
mjbae11 <mikejbae@gmail.com>
2024-12-03 16:10:36 -0800

    gitlab inheritance

---
+++ Module 303/GLAB - 303.10.1 -  Inheritance and Object Type Casting/src/Boy.java
@@ -0,0 +1,15 @@
+public class Boy extends Person
+{
+    static double ageFactor = 1.1;
+
+    public String talk()
+    {
+        return ("I have nothing to say.");
+    }
+
+    public String walk()
+    {
+        return ("I have nowhere to go.");
+    }
+}
+
